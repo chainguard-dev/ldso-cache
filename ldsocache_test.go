7b34e23b5062271610acdcd203b8abe11a1e32b9
Ariadne Conill <ariadne@dereferenced.org>
2023-01-03 17:32:49 -0600

    add real tests for parsing

--- ldsocache_test.go
+++ ldsocache_test.go
@@ -2,11 +2,13 @@ package ldsocache
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func Test_LoadCacheFile(t *testing.T) {
-	_, err := LoadCacheFile("/etc/ld.so.cache")
-	if err != nil {
-		t.Fatal()
-	}
+	cacheFile, err := LoadCacheFile("testdata/ld.so.cache")
+	require.NoError(t, err)
+	require.Equalf(t, uint32(65), cacheFile.Header.NumLibs, "there should be 65 libraries in this cache file")
+	require.Equalf(t, uint32(1421), cacheFile.Header.StrTableSize, "the string table should be 1421 bytes long")
 }
