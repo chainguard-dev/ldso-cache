431256b698f18fa45d70918eb35f821aa8d93a83
Ariadne Conill <ariadne@dereferenced.org>
2023-01-03 17:17:27 -0600

    parse the remaining data from the ld.so.cache file

--- ldsocache.go
+++ ldsocache.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -87,5 +88,52 @@ func LoadCacheFile(path string) (*LDSOCacheFile, error) {
 		rawlibs = append(rawlibs, rawlib)
 	}
 
-	return nil, nil
+	pos, err := r.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return nil, err
+	}
+
+	// The string table is a series of nul-terminated C strings.
+	strtable := make([]byte, header.StrTableSize)
+	if _, err := r.Read(strtable); err != nil {
+		return nil, err
+	}
+
+	// Now build the cache index itself.
+	entries := []LDSOCacheEntry{}
+	for _, rawlib := range rawlibs {
+		entry := LDSOCacheEntry{
+			Flags: rawlib.Flags,
+			OSVersion_Needed: rawlib.OSVersion_Needed,
+			HWCap_Needed: rawlib.HWCap_Needed,
+		}
+
+		name, err := extractShlibName(strtable, rawlib.Value - uint32(pos))
+		if err != nil {
+			return nil, err
+		}
+
+		entry.Name = name
+
+		entries = append(entries, entry)
+	}
+
+	file := LDSOCacheFile{
+		Header: header,
+		Entries: entries,
+	}
+
+	return &file, nil
+}
+
+// extractShlibName extracts a shared library from the string table.
+func extractShlibName(strtable []byte, startIdx uint32) (string, error) {
+	subset := strtable[startIdx:]
+	terminatorPos := bytes.IndexByte(subset, 0x0)
+
+	if terminatorPos == -1 {
+		return string(subset), nil
+	}
+
+	return string(subset[:terminatorPos]), nil
 }
