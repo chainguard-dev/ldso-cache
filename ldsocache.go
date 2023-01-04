6c02d755c77981cbdcfee85abf5e91f3e1ed260f
Ariadne Conill <ariadne@dereferenced.org>
2023-01-03 22:17:14 -0600

    parse cache extension sections

--- ldsocache.go
+++ ldsocache.go
@@ -10,6 +10,8 @@ import (
 
 const ldsoMagic = "glibc-ld.so.cache"
 const ldsoVersion = "1.1"
+const ldsoExtensionMagic = 0xEAA42174
+const cacheExtensionTagGenerator = uint32(1)
 
 type LDSORawCacheHeader struct {
 	Magic   [17]byte
@@ -40,15 +42,33 @@ type LDSORawCacheEntry struct {
 type LDSOCacheEntry struct {
 	Flags uint32
 
-	Name  string
+	Name string
 
 	OSVersion_Needed uint32
 	HWCap_Needed     uint64
 }
 
+type LDSOCacheExtensionHeader struct {
+	Magic uint32
+	Count uint32
+}
+
+type LDSOCacheExtensionSectionHeader struct {
+	Tag    uint32
+	Flags  uint32
+	Offset uint32
+	Size   uint32
+}
+
+type LDSOCacheExtensionSection struct {
+	Header LDSOCacheExtensionSectionHeader
+	Data   []byte
+}
+
 type LDSOCacheFile struct {
-	Header  LDSORawCacheHeader
-	Entries []LDSOCacheEntry
+	Header     LDSORawCacheHeader
+	Entries    []LDSOCacheEntry
+	Extensions []LDSOCacheExtensionSection
 }
 
 func (hdr *LDSORawCacheHeader) describe() {
@@ -57,6 +77,19 @@ func (hdr *LDSORawCacheHeader) describe() {
 	fmt.Printf("  Version [%s]\n", hdr.Version)
 	fmt.Printf("  %d library entries.\n", hdr.NumLibs)
 	fmt.Printf("  String table is %d bytes long.\n", hdr.StrTableSize)
+}
+
+func (ehdr *LDSOCacheExtensionHeader) describe() {
+	fmt.Printf("Extension header:\n")
+	fmt.Printf("  %d entries.\n", ehdr.Count)
+}
+
+func (shdr *LDSOCacheExtensionSectionHeader) describe() {
+	fmt.Printf("Extension section header:\n")
+	fmt.Printf("  Tag [%d]\n", shdr.Tag)
+	fmt.Printf("  Flags [%x]\n", shdr.Flags)
+	fmt.Printf("  Offset [%d]\n", shdr.Offset)
+	fmt.Printf("  Size [%d]\n", shdr.Size)
 }
 
 // LoadCacheFile attempts to load a cache file from disk.  When
@@ -103,12 +136,12 @@ func LoadCacheFile(path string) (*LDSOCacheFile, error) {
 	entries := []LDSOCacheEntry{}
 	for _, rawlib := range rawlibs {
 		entry := LDSOCacheEntry{
-			Flags: rawlib.Flags,
+			Flags:            rawlib.Flags,
 			OSVersion_Needed: rawlib.OSVersion_Needed,
-			HWCap_Needed: rawlib.HWCap_Needed,
+			HWCap_Needed:     rawlib.HWCap_Needed,
 		}
 
-		name, err := extractShlibName(strtable, rawlib.Value - uint32(pos))
+		name, err := extractShlibName(strtable, rawlib.Value-uint32(pos))
 		if err != nil {
 			return nil, err
 		}
@@ -118,10 +151,64 @@ func LoadCacheFile(path string) (*LDSOCacheFile, error) {
 		entries = append(entries, entry)
 	}
 
+	// Extension data begins at the next 4-byte aligned position.
+	pos, err = r.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return nil, err
+	}
+
+	// Align to nearest 4 byte boundary.
+	alignedPos := (pos & -16) + 8
+	pos, err = r.Seek(alignedPos, io.SeekStart)
+	if err != nil {
+		return nil, err
+	}
+
 	file := LDSOCacheFile{
-		Header: header,
+		Header:  header,
 		Entries: entries,
 	}
+
+	// Check for a cache extension section.
+	extHeader := LDSOCacheExtensionHeader{}
+	if err := binary.Read(r, binary.LittleEndian, &extHeader); err != nil {
+		return &file, nil
+	}
+	if extHeader.Magic != ldsoExtensionMagic {
+		return &file, nil
+	}
+	extHeader.describe()
+
+	// Parse the extension chunks we understand.
+	sections := []LDSOCacheExtensionSection{}
+	for i := uint32(0); i < extHeader.Count; i++ {
+		sectionHeader := LDSOCacheExtensionSectionHeader{}
+		if err := binary.Read(r, binary.LittleEndian, &sectionHeader); err != nil {
+			return &file, nil
+		}
+		sectionHeader.describe()
+
+		section := LDSOCacheExtensionSection{Header: sectionHeader}
+		sections = append(sections, section)
+	}
+
+	// Load extension data.
+	for _, section := range sections {
+		pos, err = r.Seek(int64(section.Header.Offset), io.SeekStart)
+		if err != nil {
+			return &file, nil
+		}
+		if pos != int64(section.Header.Offset) {
+			return &file, nil
+		}
+
+		section.Data = make([]byte, section.Header.Size)
+		if _, err := r.Read(section.Data); err != nil {
+			return &file, nil
+		}
+	}
+
+	file.Extensions = sections
 
 	return &file, nil
 }
